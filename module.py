2b5d9fdde2c32b6f31cb6e1b67c8417616eb7616
Philippe16 <philippe.suenson@hotmail.com>
2022-11-18 13:32:07 +0100

    my_notebook

---
+++ module.py
@@ -0,0 +1,6 @@
+def listOfNames();
+    names =["Chris", "Bent", "Brian", "Torben"]
+    return names
+
+if _name_ == '__main__':
+    listOfNames()
---
+++ module2.py
@@ -0,0 +1,5 @@
+import module
+def printNames():
+    
+    if __name__ -- '__main__':
+        listOfNames()
